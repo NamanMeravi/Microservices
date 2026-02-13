436ebfc0e5f19b5bb8f387404209ea061ed3f050
NamanMeravi <namanmeravi0@gmail.com>
2026-02-14 00:44:52 +0530

    backend started
    update on user service

---
+++ backend/user/src/config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+export const DbConnect = async()=>{
+    const url = process.env.MONGO_URL;
+
+    if(!url){
+        throw new Error("MONGO_URI is not defined in enviroment variable")
+
+    }
+
+    try {
+        await mongoose.connect(url,{
+            dbName:"Microservice"
+        }).then(()=>{
+            console.log("connected to Mongodb");
+            
+        })
+    } catch (error) {
+        console.error("failed to connect to database");
+        process.exit(1);
+        
+    }
+}
